4122cdc44bd393d775a51f48149e2af0f7111b03
Elite-Oye <oyewolensikan8@gmail.com>
2026-05-27 20:46:15 +0100

    feature : add Auth APIs (register, login and logout)

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+const app = express();
+
+app.use(express.json());
+
+
+
+//import routes
+
+import userRouter from './routes/user.route.js';
+import postRouter from './routes/user.route.js';
+//routes declaration 
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter)
+
+//example route: http://localhost:4000/api/v1/users/register
+export default app;
